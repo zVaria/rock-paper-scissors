1d8bbe6fc16c099d726f70e911afcb34cdc5a4fd
Alan <alanzioom@gmail.com>
2022-08-01 23:23:20 -0500

    added game rules and rng computer selection

---
+++ game.js
@@ -0,0 +1,40 @@
+function getComputerChoice() {
+    let randomAssignment = Math.floor(Math.random() * 3);
+    let computerChoice
+
+    if(randomAssignment === 0) {
+        computerChoice = "rock"
+    } else if (randomAssignment === 1) {
+        computerChoice = "paper"
+    } else {
+        computerChoice = "scissors"
+    }
+
+    return computerChoice;
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    // Rock vs Paper, Rock vs Scissors, Rock vs Rock
+    if((playerSelection == "rock") && (computerSelection == "paper")) 
+        return "You lose! Paper beats Rock"
+    else if((playerSelection == "rock") && (computerSelection == "scissors"))
+        return "You win! Rock beats Scissors"
+    else if((playerSelection == "rock") && (computerSelection == "rock"))
+        return "Tie game!"
+    // Scissors vs Paper, Scissors vs Scissors, Scissors vs Rock
+    else if((playerSelection == "scissors") && (computerSelection == "paper"))
+        return "You win! Scissors beats Paper"
+    else if((playerSelection == "scissors") && (computerSelection == "scissors")) 
+        return "Tie game!"
+    else if ((playerSelection == "scissors") && (computerSelection == "rock"))
+        return "You lose! Rock beats Scissors"
+    // Paper vs Paper, Paper vs Scissors, Paper vs Rock
+    else if((playerSelection == "paper") && (computerSelection == "paper"))
+        return "Tie game!"
+    else if((playerSelection == "paper") && (computerSelection == "scissors"))
+        return "You lose! Scissors beats Paper"
+    else 
+        return "You win! Paper beats Rock"
+}
